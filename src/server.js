a359cc8bda7dbd4160a33bde50d33d4a243482f7
Edward Blanciak <blanciak.edward2@gmail.com>
2020-04-09 10:16:30 -0400

    done

--- src/server.js
+++ src/server.js
@@ -12,4 +12,17 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// NOTE controller function is the callback
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
